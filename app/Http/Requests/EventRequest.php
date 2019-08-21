775ebc07bc5ba507c7cd443d7838e546b53d8b06
rafaeldamiam <41579736+rafaeldamiam@users.noreply.github.com>
2019-08-21 15:39:00 -0300

    Inicio das validaçoes

---
+++ app/Http/Requests/EventRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EventRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     * https://laravel.com/docs/master/validation#available-validation-rules
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'Nome' => 'required',
+            'DataInicio' => 'required',
+            'DataFim' => 'required',
+            'DataLimiteInscricao' => 'required',
+            'ConteudoProgramatico' => 'required',
+            'Responsavel' => 'required',
+            'CargaHoraria' => 'required',
+            'HorarioInicio' => 'required',
+            'HorarioFim' => 'required',
+            'Local' => 'required',
+            'Logo' => 'required',
+        ];
+    }
+}
